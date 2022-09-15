7f424b12a487d55d11531d79c1825fe03f704459
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-14 23:29:48 -0300

    divide authSchema em newUser e user Schemas

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,9 +1,14 @@
 import joi from "joi";
 
-const authSchema = joi.object({
+const newUser = joi.object({
     email: joi.string().email().required(),
     password: joi.string().required(),
     confirmPassword: joi.string().required().valid(joi.ref('password'))
 });
 
-export default authSchema
+const user = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required()
+});
+
+export default { newUser, user }
