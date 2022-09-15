0bb2f549e41ec76a03e8117d77761918d6ff2b0c
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-14 21:42:07 -0300

    implementa index router

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import 'express-async-errors';
+import router from './routers';
 import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
 dotenv.config();
 
 const server = express();
 server.use(express.json());
 server.use(cors());
+server.use(router);
 server.use(errorHandlerMiddleware)
 
 server.listen(process.env.PORT, () => console.log(`
