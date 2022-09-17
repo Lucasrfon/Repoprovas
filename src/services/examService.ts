ba2cf8ebdb8e730364e761c82932ce99f6ed2490
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-16 23:08:20 -0300

    implementa service para cadastro de prova

---
+++ src/services/examService.ts
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+import { findDiscipline, findExamCategory, findTeacher, findTeacherDiscipline, registerExam } from '../repositories/examRepository';
+import { TExam } from '../types/examTypes';
+dotenv.config();
+
+export async function insertExam(exam: TExam) {
+    const category = await findExamCategory(exam.category);
+    const teacher = await findTeacher(exam.teacher);
+    const discipline = await findDiscipline(exam.discipline);
+    
+    if(!category || !teacher || !discipline) {
+        throw { type: "not found", message: 'Categoria, instrutor ou matéria não cadastrados'}
+    }
+    
+    const teachersDiscipline = await findTeacherDiscipline(teacher.id, discipline.id);
+
+    if(!teachersDiscipline) {
+        throw { type: "not found", message: 'Categoria, instrutor ou matéria não cadastrados'}
+    }
+
+    const newExam = {
+        name: exam.name,
+        pdfUrl: exam.pdfUrl, 
+        categoryId: category.id,
+        teacherDisciplineId: teachersDiscipline.id
+    }
+    
+    await registerExam(newExam);
+}
