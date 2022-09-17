acee622ccd7dbada6694fab429faec32c631f9f3
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-17 00:58:43 -0300

    importa getExamsByGroupByDiscipline no examRouter

--- src/routers/examRouter.ts
+++ src/routers/examRouter.ts
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { registerNewExam } from "../controllers/examController";
+import { getExamsGroupByDiscipline, registerNewExam } from "../controllers/examController";
 import validateSchema from "../middlewares/validateSchema";
 import validateToken from "../middlewares/validateToken";
 import examSchema from "../schemas/examSchema";
@@ -8,7 +8,7 @@ const examRouter = Router();
 
 examRouter.use(validateToken())
 examRouter.post('/exams', validateSchema(examSchema), registerNewExam);
-examRouter.get('/exams/subject');
+examRouter.get('/exams/discipline', getExamsGroupByDiscipline);
 examRouter.get('/exams/teacher');
 
 export default examRouter;
