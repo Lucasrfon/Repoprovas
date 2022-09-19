c13ba89b3b94dff31b6db269f40cce76156faef8
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-19 13:29:19 -0300

    implementa testes da rota /signup no auth.test

---
+++ tests/auth.test.ts
@@ -0,0 +1,56 @@
+import supertest from "supertest";
+import app from "../src/app";
+import client from "../src/dbStrategy/prisma";
+import loginFactory from "./factories/loginFactory";
+
+beforeEach(async () => {
+    await client.$executeRaw`TRUNCATE TABLE users;`;
+  });
+
+afterAll(async () => {
+    await client.$disconnect();
+});
+
+describe('POST /signup', () => {
+    
+    it('Sucesso deve retornar status 201', async () => {
+        const user = await loginFactory();
+        const newUser = {...user, confirmPassword: user.password}
+
+        const result = await supertest(app).post("/signup").send(newUser);
+        const status = result.status;
+
+        expect(status).toBe(201)
+    });
+
+    it('Schema incompleto deve retornar 422', async () => {
+        const user = loginFactory();
+
+        const result = await supertest(app).post("/signup").send(user);
+        const status = result.status;
+
+        expect(status).toBe(500);
+        expect(result.body).toBeInstanceOf(Object);
+    });
+
+    it('Email já cadastrado deve retornar 409', async () => {
+        const user = loginFactory();
+        const newUser = {...user, confirmPassword: (await user).password}
+        await supertest(app).post("/signup").send(newUser);
+
+        const result = await supertest(app).post("/signup").send(newUser);
+        const status = result.status;
+
+        expect(status).toBe(500)    
+    });
+});
+
+// describe('POST /login', () => {
+//     it('token inválido, deveria retornar 401', async () => {
+
+//         const result = await supertest(app).post("/login").send();
+//         const status = result.status;
+
+//         expect(status).toBe(500)    
+//     });
+// });
